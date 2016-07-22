0babfc97fd6e434f5930f8da9a09e824faf3b2bb
Egil <egil.moller@freecode.no>
2016-07-23 01:42:58 +0200

    New syntax for run() that handles redirects sanely

--- pieshell/cmdline.py
+++ pieshell/cmdline.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python
 
-import subprocess
-import threading
 import os
 import fcntl
 import types
@@ -18,10 +16,9 @@ try:
 except:
     MAXFD = 256
 
-class ShellScript(object):
-    pass
+debug = False
 
-class Environment(ShellScript):
+class Environment(object):
     def __init__(self, cwd = None, env = None, interactive = False):
         self.cwd = os.getcwd()
         if cwd is not None:
@@ -62,30 +59,120 @@ class RunningPipeline(object):
     def __init__(self, processes):
         self.processes = processes
     def __iter__(self):
-        return iterio.LineInputHandler(self.processes[-1].pipes['stdout'])
+        return iterio.LineInputHandler(self.processes[-1].redirects.stdout.pipe)
     def join(self):
         last = self.processes[-1]
         last.wait()
 
-class Pipeline(ShellScript):
-    interactive_state = threading.local()
+class PIPE(object): pass
 
+class Redirect(object):
+    fd_names = {"stdin": 0, "stdout": 1, "stderr": 2}
+    fd_flags = {0: os.O_RDONLY, 1: os.O_WRONLY, 2: os.O_WRONLY}
+    def __init__(self, fd, source = None, flag = None, mode = 0777, pipe=None):
+        if isinstance(fd, Redirect):
+            fd, source, flag, mode, pipe = fd.fd, fd.source, fd.flag, fd.mode, fd.pipe
+        if not isinstance(fd, int):
+            fd = self.fd_names[fd]
+        if flag is None:
+            flag = self.fd_flags[fd]
+        self.fd = fd
+        self.source = source
+        self.flag = flag
+        self.mode = mode
+        self.pipe = pipe
+    def open(self):
+        source = self.source
+        if not isinstance(source, int):
+            source = os.open(source, self.flag, self.mode)
+        return source
+    def close_source_fd(self):
+        # FIXME: Only close source fds that come from pipes instead of this hack...
+        if isinstance(self.source, int) and self.source > 2:
+            os.close(self.source)
+    def perform(self):
+        source = self.open()
+        if source != self.fd:
+            os.dup2(source, self.fd)
+    def make_pipe(self):
+        if self.source is not PIPE: return self
+        rfd, wfd = pipe.pipe_cloexec()
+        if self.flag & os.O_RDONLY:
+            pipefd, sourcefd = wfd, rfd
+        else:
+            sourcefd, pipefd = wfd, rfd
+        return type(self)(self.fd, sourcefd, self.flag, self.mode, pipefd)
+    def __repr__(self):
+        flagmode = []
+        if self.flag not in (os.O_RDONLY, os.O_WRONLY):
+            flagmode.append("f=%s" % self.flag)
+        if self.mode != 0777:
+            flagmode.append("m=%s" % self.mode)
+        if flagmode:
+            flagmode = "[" + ",".join(flagmode) + "]"
+        else:
+            flagmode = ""
+        if self.flag & os.O_RDONLY:
+            arrow = "<-%s-" % flagmode
+        else:
+            arrow = "-%s->" % flagmode
+        items = [str(self.fd), arrow, str(self.source)]
+        if self.pipe is not None:
+            items.append("pipe=%s" % self.pipe)
+        return " ".join(items)
+
+class Redirects(object):
+    def __init__(self, *redirects):
+        self.redirects = {}
+        if redirects and isinstance(redirects[0], Redirects):
+            for redirect in redirects[0].redirects.values():
+                self.register(Redirect(redirect))
+        else:
+            self.redirect(0, 0)
+            self.redirect(1, 1)
+            self.redirect(2, 2)
+            for redirect in redirects:
+                self.register(redirect)
+    def register(self, redirect):
+        if not isinstance(redirect, Redirect):
+            redirect = Redirect(redirect)
+        if redirect.source is None:
+            del self.redirects[redirect.fd]
+        else:
+            self.redirects[redirect.fd] = redirect
+        return self
+    def redirect(self, *arg, **kw):
+        self.register(Redirect(*arg, **kw))
+        return self
+    def make_pipes(self):
+        return type(self)(*[redirect.make_pipe()
+                            for redirect in self.redirects.itervalues()])
+    def perform(self):
+        for redirect in self.redirects.itervalues():
+            redirect.perform()
+        self.close_other_fds()
+    def close_other_fds(self):
+        # FIXME: Use os.closerange if available
+        for i in xrange(0, MAXFD):
+            if i in self.redirects: continue
+            try:
+                os.close(i)
+            except:
+                pass
+    def close_source_fds(self):
+        for redirect in self.redirects.itervalues():
+            redirect.close_source_fd()
+    def __getattr__(self, name):
+        return self.redirects[Redirect.fd_names[name]]
+    def __repr__(self):
+        redirects = self.redirects.values()
+        redirects.sort(lambda a, b: cmp(a.fd, b.fd))
+        return ", ".join(repr(redirect) for redirect in redirects)
+
+class Pipeline(object):
+    interactive_state = threading.local()
     def __init__(self, env):
         self.env = env
-
-    def setup_run_pipes(self, stdin = None, stdout = None, stderr = None, *arg, **kw):
-        inputs = {'stdin': stdin,
-                  'stdout': stdout,
-                  'stderr': stderr}
-        pipes = {}
-        for key in ('stdin', 'stdout', 'stderr'):
-            if inputs[key] is subprocess.PIPE:
-                r, w = pipe.pipe_cloexec()
-                if key == 'stdin':
-                    pipes[key], inputs[key] = w, r
-                else:
-                    inputs[key], pipes[key] = w, r
-        return arg, kw, inputs, pipes
     def _coerce(self, thing):
         if isinstance(thing, Pipeline):
             return thing
@@ -98,17 +185,19 @@ class Pipeline(ShellScript):
     def __or__(self, other):
         return Pipe(self.env, self, self._coerce(other))
     def __gt__(self, file):
-        return Redirect(self.env, self, file, "stdout")
+        return CmdRedirect(self.env, self, file, "stdout")
     def __lt__(self, file):
-        return Redirect(self.env, self, file, "stdin")
+        return CmdRedirect(self.env, self, file, "stdin")
     def __add__(self, other):
         return Group(self.env, self, other)
-    def run(self, stdin = None, stdout = subprocess.PIPE, stderr = None, **kw):
-        return RunningPipeline(self._run(stdin = stdin, stdout = stdout, stderr = stderr, **kw))
+    def run(self, redirects = []):
+        if not isinstance(redirects, Redirects):
+            redirects = Redirects(*redirects)
+        return RunningPipeline(self._run(redirects))
     def __iter__(self):
-        return iter(self.run(stdout=subprocess.PIPE))
+        return iter(self.run([Redirect("stdout", PIPE)]))
     def __unicode__(self):
-        return "\n".join(iter(self.run(stdout=subprocess.PIPE)))
+        return "\n".join(iter(self.run([Redirect("stdout", PIPE)])))
     @classmethod
     def repr(cls, obj):
         cls.interactive_state.repr = True
@@ -118,7 +207,7 @@ class Pipeline(ShellScript):
             cls.interactive_state.repr = False
     def __repr__(self):
         if self.env.interactive and not getattr(self.interactive_state, "repr", False):
-            pipeline = self.run(stdout=sys.stdout.fileno(), stderr=sys.stderr.fileno())
+            pipeline = self.run()
             try:
                 iterio.IOHandlers.delay_cleanup()
                 try:
@@ -162,27 +251,15 @@ class Command(Pipeline):
                     os.close(i)
                 except:
                     pass
-    def _child(self, *args, **kw):
-        for key in ("stdin", "stdout", "stderr"):
-            fd = kw.get(key, None)
-            if fd is not None:
-                if not isinstance(fd, int):
-                    fd = os.open(fd, {'stdin': os.O_RDONLY,
-                                      'stdout': os.O_WRONLY,
-                                      'stderr': os.O_WRONLY}[key])
-                dst = {"stdin": 0, "stdout": 1, "stderr": 2}[key]
-                if fd != dst:
-                    os.dup2(fd, dst)
-        self._close_fds()
-
+    def _child(self, redirects, args):
+        redirects.perform()
         os.chdir(self.env.cwd)
         os.execvpe(args[0], args, self.env.env)
         os._exit(-1)
 
-    def _run(self, *args, **kw):
-        args, kw, inputs, pipes = self.setup_run_pipes(*args, **kw)
-        kw.update(inputs)
-        print "Running %s with inputs=%s, pipes=%s, %s, %s" % (Pipeline.repr(self), inputs, pipes, Pipeline.repr(args), Pipeline.repr(kw))
+    def _run(self, redirects):
+        redirects = redirects.make_pipes()
+        if debug: print "Running %s with %s" % (Pipeline.repr(self), repr(redirects))
 
         named_pipes = {}
         def handle_named_pipe(thing):
@@ -202,7 +279,6 @@ class Command(Pipeline):
             named_pipes[name] = (direction, thing)
             
             def clean_named_pipe():
-                print 'REMOVE', name
                 os.unlink(name)
 
             iterio.IOHandlers.register_cleanup(clean_named_pipe)
@@ -218,7 +294,7 @@ class Command(Pipeline):
 
         pid = os.fork()
         if pid == 0:
-            self._child(*args, **kw)
+            self._child(redirects, args)
 
         class Proc(object):
             def __init__(self, pid):
@@ -227,19 +303,15 @@ class Command(Pipeline):
                 os.waitpid(self.pid, 0)
         res = Proc(pid)
 
-        for fd in inputs.itervalues():
-            if isinstance(fd, int) and fd > 2:
-                os.close(fd)
+        redirects.close_source_fds()
 
         for name, (direction, thing) in named_pipes.iteritems():
             if direction == 'w':
-                thing._run(stdout=name)
+                thing._run([Redirect("stdout", name)])
             else:
-                thing._run(stdin=name)
+                thing._run([Redirect("stdin", name)])
 
-        res.pipes = pipes
-        for fd in pipes.itervalues():
-            pass
+        res.redirects = redirects
 
         return [res]
 
@@ -262,9 +334,9 @@ class Function(Pipeline):
         else:
             return repr(thing)
 
-    def _run(self, *arg, **kw):
-        arg, kw, inputs, pipes = self.setup_run_pipes(*arg, **kw)
-        print "Running %s with inputs=%s, pipes=%s, %s, %s" % (Pipeline.repr(self), inputs, pipes, Pipeline.repr(arg), Pipeline.repr(kw))
+    def _run(self, redirects):
+        redirects = redirects.make_pipes()
+        if debug: print "Running %s with %s" % (Pipeline.repr(self), repr(redirects))
 
         def convert(x):
             if isinstance(x, str):
@@ -277,16 +349,16 @@ class Function(Pipeline):
         thing = self.function
         if isinstance(thing, types.FunctionType):
             thing = thing(
-                iterio.LineInputHandler(inputs['stdin']),
+                iterio.LineInputHandler(redirects.stdin.open()),
                 *self.arg, **self.kw)
         if hasattr(thing, "__iter__"):
             thing = iter(thing)
 
         res = iterio.LineOutputHandler(
-            inputs['stdout'],
+            redirects.stdout.open(),
             (convert(x) for x in thing))
 
-        res.pipes = pipes
+        res.redirects = redirects
 
         return [res]
         
@@ -298,10 +370,10 @@ class Pipe(Pipeline):
         self.dst = dst
     def _repr(self):
         return u"%s | %s" % (repr(self.src), repr(self.dst))
-    def _run(self, stdin = None, stdout = None, stderr = None, *arg, **kw):
-        print "Running %s with stdin=%s, stdout=%s, stderr=%s, %s, %s" % (Pipeline.repr(self), stdin, stdout, stderr, Pipeline.repr(arg), Pipeline.repr(kw))
-        src = self.src._run(stdin=stdin, stdout=subprocess.PIPE, stderr=stderr, *arg, **kw)
-        dst = self.dst._run(stdin=src[-1].pipes['stdout'], stdout=stdout, stderr=stderr, *arg, **kw)
+    def _run(self, redirects):
+        if debug: print "Running %s with %s" % (Pipeline.repr(self), repr(redirects))
+        src = self.src._run(Redirects(redirects).redirect("stdout", PIPE))
+        dst = self.dst._run(Redirects(redirects).redirect("stdin", src[-1].redirects.stdout.pipe))
         return src + dst
 
 # class Group(Pipeline):
@@ -315,7 +387,7 @@ class Pipe(Pipeline):
 #     def _repr(self):
 #         return u"%s + %s" % (repr(self.first), repr(self.second))
 
-class Redirect(Pipeline):
+class CmdRedirect(Pipeline):
     def __init__(self, env, pipeline, file, filedescr):
         self.env = env
         self.pipeline = pipeline
@@ -328,7 +400,7 @@ class Redirect(Pipeline):
             sep = ">"
         return u"%s %s %s" % (repr(self.pipeline), sep, self.file)
     def _run(self, *arg, **kw):
-        print "Running %s with %s=%s, %s, %s" % (Pipeline.repr(self), self.filedescr, Pipeline.repr(arg), Pipeline.repr(kw))
+        if debug: print "Running %s with %s=%s, %s, %s" % (Pipeline.repr(self), self.filedescr, Pipeline.repr(arg), Pipeline.repr(kw))
         if self.filedescr == 'stdin':
             stdin = fd = os.open(self.file, os.O_RDONLY)
         elif self.filedescr == 'stdout':
