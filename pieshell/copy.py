e955dafd566a63ec1ca524de2df9f5dd343ae8b9
Egil <egil.moller@freecode.no>
2016-07-27 12:40:02 +0200

    Added deep copy module

---
+++ pieshell/copy.py
@@ -0,0 +1,31 @@
+# Minimal reimplementation of copy.deepcopy that happily copies
+# objects inheriting from type...
+
+copy_state = threading.local()
+
+@contextlib.contextmanager
+def copy_session():
+    old_memo = getattr(copy_state, "memo", None)
+    copy_state.memo = old_memo or {}
+    try:
+        yield copy_state.memo
+    finally:
+        copy_state.memo = old_memo
+
+def deepcopy(obj):
+    with copy_session() as memo:
+        key = id(obj)
+        if key not in memo:
+            if isinstance(obj, (list, tuple)):
+                memo[key] = type(obj)(deepcopy(item) for item in obj)
+            elif isinstance(obj, dict):
+                memo[key] = type(obj)({deepcopy(key):deepcopy(value) for key, value in obj.iteritems()})
+            elif isinstance(obj, str):
+                memo[key] = str(obj)
+            elif isinstance(obj, unicode):
+                memo[key] = unicode(obj)
+            elif hasattr(obj, "__deepcopy__"):
+                memo[key] = obj.__deepcopy__()
+            else:
+                memo[key] = obj
+        return memo[key]
