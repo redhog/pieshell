45d7f72dcbba9790e03e77c0430524d93f449a02
Egil <egil.moller@freecode.no>
2016-08-01 15:36:24 +0200

    Bugfix for files included in build

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
@@ -20,7 +20,7 @@ It can be used in two major ways:
     author_email = "egil.moller@piratpartiet.se",
     license = "GPL",
     url = "https://github.com/redhog/pieshell",
-    packages = ["pieshell"],
+    packages = find_packages(),
     entry_points={
         'console_scripts': [
             'pieshell = pieshell.shell:main',
