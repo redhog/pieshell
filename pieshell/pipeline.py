3596dd2b7fa98d2aacf15c5c95b60f0fec54c258
Egil <egil.moller@freecode.no>
2016-08-15 06:55:07 +0200

    Better repr for running processes

--- pieshell/pipeline.py
+++ pieshell/pipeline.py
@@ -50,23 +50,38 @@ class RunningPipeline(object):
     def __repr__(self):
         return repr(self.pipeline)
 
-class RunningProcess(iterio.ProcessSignalHandler):
+class RunningItem(object):
+    def __init__(self, cmd, iohandler):
+        self.cmd = cmd
+        self.iohandler = iohandler
+    def __getattr__(self, name):
+        return getattr(self.iohandler, name)
+
+class RunningFunction(RunningItem):
+    def __repr__(self):
+        return '%s(%s)' % (self.cmd._function_name(), ",".join(self.iohandler._repr_args()))
+
+class RunningProcess(RunningItem):
+    def __init__(self, cmd, pid):
+        RunningItem.__init__(self, cmd, iterio.ProcessSignalHandler(pid))
     def __repr__(self):
         status = []
-        last_event = self.last_event
+        last_event = self.iohandler.last_event
         if last_event:
             last_event = iterio.siginfo_to_names(last_event)
-        if self.is_running:
+        if self.iohandler.is_running:
             if last_event and last_event["ssi_code"] != 'CLD_CONTINUED':
-                status.append("status=%s" % self.last_event["ssi_code"])
-                status.append("signal=%s" % self.last_event["ssi_status"])
+                status.append("status=%s" % self.iohandler.last_event["ssi_code"])
+                status.append("signal=%s" % self.iohandler.last_event["ssi_status"])
         else:
-            status.append("exit_code=%s" % self.last_event["ssi_status"])
+            status.append("exit_code=%s" % self.iohandler.last_event["ssi_status"])
         if status:
             status = ' (' + ', '.join(status) + ')'
         else:
             status = ''
-        return '%s%s' % (self.pid, status)
+        return '%s%s' % (self.iohandler.pid, status)
+
+
 
 # help() doesn't let you override help on objects, only on classes, so
 # make everything a class...
@@ -381,7 +396,7 @@ class Command(BaseCommand):
 
         log.log(indentation + "  %s: Command line %s with %s" % (pid, ' '.join(repr(arg) for arg in args), repr(redirects)), "cmd")
 
-        proc = RunningProcess(pid)
+        proc = RunningProcess(self, pid)
         self._running_processes.append(proc)
 
         redirects.close_source_fds()
@@ -420,18 +435,30 @@ class Function(Pipeline):
         self._kw = kw
     def __deepcopy__(self, memo = {}):
         return type(self)(self._env, self.__dict__["function"], *copy.deepcopy(self._arg), **copy.deepcopy(self._kw))
+    def _function_name(self):
+        thing = self.__dict__["function"] # Don't wrap functions as instance methods
+        if isinstance(thing, (types.FunctionType, types.MethodType)):
+            mod = thing.__module__ or ''
+            if mod:
+                mod = mod + '.'
+            
+            if isinstance(thing, types.MethodType):
+                func = "%s.%s" % (thing.im_class.__name__, thing.im_func.func_name)
+            else:
+                func = thing.func_name
+
+            return u"%s%s" % (mod, func)
+        else:
+            return repr(thing)
     def _repr(self):
         thing = self.__dict__["function"] # Don't wrap functions as instance methods
-        if isinstance(thing, types.FunctionType):
+        if isinstance(thing, (types.FunctionType, types.MethodType)):
             args = []
             if self._arg:
                 args += [repr(arg) for arg in self._arg]
             if self._kw:
                 args += ["%s=%s" % (key, repr(value)) for (key, value) in self._kw.iteritems()]
-            mod = thing.__module__ or ''
-            if mod:
-                mod = mod + '.'
-            return u"%s%s(%s)" % (mod, thing.func_name, ','.join(args))
+            return u"%s(%s)" % (self._function_name(), ','.join(args))
         else:
             return repr(thing)
 
@@ -461,11 +488,13 @@ class Function(Pipeline):
         if hasattr(thing, "__iter__"):
             thing = iter(thing)
 
-        self._running_process = iterio.LineOutputHandler(
-            redirects.stdout.open(),
-            (convert(x) for x in thing),
-            borrowed=redirects.stdout.borrowed)
-        
+        self._running_process = RunningFunction(
+            self,
+            iterio.LineOutputHandler(
+                redirects.stdout.open(),
+                (convert(x) for x in thing),
+                borrowed=redirects.stdout.borrowed))
+            
         self._redirects = self._running_process.redirects = redirects
 
         return [self._running_process]
