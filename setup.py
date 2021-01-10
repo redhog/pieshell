2cd18e15e6d565ee8b8d9a7d8d12d805c253f7dc
Egil <egil.moller@freecode.no>
2021-01-10 18:38:35 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 import os.path
 
-VERSION = "0.0.7"
+VERSION = "0.0.8"
 
 with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as rf:
     with open(os.path.join(os.path.dirname(__file__), "pieshell", "README.md"), "w") as wf:
