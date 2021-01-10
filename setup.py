0e99d6f7452d52ae6d59b1de332238c17774456c
Egil <egil.moller@freecode.no>
2021-01-10 19:41:23 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -3,18 +3,21 @@
 from setuptools import setup, find_packages
 import os.path
 
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 
 with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as rf:
     with open(os.path.join(os.path.dirname(__file__), "pieshell", "README.md"), "w") as wf:
-        wf.write(rf.read())
-
+        README = rf.read()
+        wf.write(README)
+        
 with open(os.path.join(os.path.dirname(__file__), "pieshell", "version.py"), "w") as wf:
     wf.write("version = '%s'\n" % (VERSION,)) 
     
 setup(
     name = "pieshell",
     description = "Pieshell is a Python shell environment that combines the expressiveness of shell pipelines with the prower of python iterators. It can be used both as an interactive shell and as an ordinary python module replacing e.g. subprocess.Popen",
+    long_description_content_type = "text/markdown",
+    long_description = README,
     keywords = "Python shell pipelines suprocess",
     install_requires = ["signalfd"],
     version = VERSION,
