6ad114841ef3fc82b2469e6f49442ab02ae31f38
Egil <egil.moller@freecode.no>
2021-01-19 23:02:30 +0100

    Some basic ^Z-handling

--- pieshell/pipeline/base.py
+++ pieshell/pipeline/base.py
@@ -140,6 +140,10 @@ class Pipeline(DescribableObject):
                 self.run_interactive()
             except KeyboardInterrupt as e:
                 log.log("Canceled:\n%s" % (e,), "error")
+            except running.PipelineInterrupted as e:
+                log.log("Canceled:\n%s" % (e,), "error")
+            except running.PipelineSuspended as e:
+                log.log("Suspended:\n%s" % (e,), "error")
             except Exception as e:
                 log.log("Error:\n%s" % (e,), "error")
                 sys.last_traceback = sys.exc_info()[2]
