d494489ffcf9b126f5573ece1235d46035e4e1e7
Egil <egil.moller@freecode.no>
2023-10-29 19:23:38 +0100

    More tests

---
+++ tests/test_repr.py
@@ -0,0 +1,22 @@
+import unittest
+import pieshell
+import sys
+import os
+
+dir = os.path.dirname(__file__)
+sys.path[0:0] = [dir]
+pieshell.envScope["env"]._cd(dir)
+
+
+class TestRepr(unittest.TestCase):
+    def test_command_repr(self):
+        e = pieshell.env
+        l = e.ls('-l') | None
+        assert "ls('-l') with 1 --> /dev/null in" in repr(l)
+        
+    def test_running_command_repr(self):
+        e = pieshell.env
+        p = (e.ls("-l") | None).run_interactive()
+        assert "ls('-l') as" in repr(p)
+        assert "with 1 --> /dev/null" in repr(p)
+        assert "(exit_code=0)" in repr(p)
