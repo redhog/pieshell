bb5e209d1cfcf8d629bc7b9c1ce384e2580c9143
Egil <egil.moller@freecode.no>
2024-06-13 23:14:06 +0200

    Added jedi optional dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 import os.path
 
-VERSION = "0.2.12"
+VERSION = "0.2.13"
 
 with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as rf:
     with open(os.path.join(os.path.dirname(__file__), "pieshell", "README.md"), "w") as wf:
@@ -22,7 +22,9 @@ setup(
     install_requires = ['python-slugify'],
     extras_require = {
         'linux': ['signalfd'],
-        'ps': ['psutil']
+        'ps': ['psutil'],
+        'completion': ['jedi'],
+        'ptpython': ['ptpython']
     },
     version = VERSION,
     author = "Egil Moeller",
